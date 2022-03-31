cc9755ce57452f0dc1d01dffae521463a32e2747
Ezgiackgz <75390977+Ezgiackgz@users.noreply.github.com>
2022-03-31 22:45:58 +0300

    update

--- ui.js
+++ ui.js
@@ -18,3 +18,19 @@ UI.prototype.clearInputs = function (e1, e2, e3) {
   e2.value = "";
   e3.value = "";
 };
+
+UI.prototype.displayMessages = function (message, type) {
+  const cardBody = document.querySelector(".card-body");
+  //Alert Divi
+
+  const div = document.createElement("div");
+
+  div.className = `alert alert-${type}`;
+  div.textContent = message;
+
+  cardBody.appendChild(div);
+
+  setTimeout(function () {
+    div.remove();
+  }, 2000);
+};
