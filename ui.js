12fd97bc043188a0799ed537439e12a1a15a3d99
Ezgiackgz <75390977+Ezgiackgz@users.noreply.github.com>
2022-03-31 15:01:25 +0300

    update

--- ui.js
+++ ui.js
@@ -1,4 +1,20 @@
 function UI() {}
 UI.prototype.addFilmTOUI = function (newFilm) {
-    console.log(newFilm);
+  // console.log(newFilm);
+  const filmList = document.getElementById("films");
+  // console.log(filmList);
+  filmList.innerHTML += `
+ <tr>
+        <td><img src="${newFilm.url}" class="img-fluid img-thumbnail"></td>
+        <td>${newFilm.title}</td>
+        <td>${newFilm.director}</td>
+       <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
+ </tr>
+ `;
+};
+//İnput Alanını boşalt
+UI.prototype.clearInputs = function (e1, e2, e3) {
+  e1.value = "";
+  e2.value = "";
+  e3.value = "";
 };
