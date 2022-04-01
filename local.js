137ade1c30c0211263f8537c7e7333595f7bf160
Ezgiackgz <75390977+Ezgiackgz@users.noreply.github.com>
2022-04-02 00:25:52 +0300

    finish

--- local.js
+++ local.js
@@ -1,27 +1,32 @@
 function Storage() {}
 Storage.prototype.addFilmToStorage = function (newFilm) {
-  // console.log(newFilm);
+  let films = this.getFilmsFromStorage();
 
-  let films = this.getFilmsFormStorage();
   films.push(newFilm);
-  //film objeyi diziye ataık örneğin
-  /*
-  [
-      {title:"asdsads",director:"sdfsdf",url:"sdfsdf"},
-      {title:"asdsads",director:"sdfsdf",url:"sdfsdf"}
-
-  ]
-  */
   localStorage.setItem("films", JSON.stringify(films));
 };
 Storage.prototype.getFilmsFromStorage = function () {
   let films;
+
   if (localStorage.getItem("films") === null) {
-    //eğer films adında bir key yoksa boş bir dizi oluştur
     films = [];
   } else {
-    //var ise json formatında dizi haline çevirerek al
     films = JSON.parse(localStorage.getItem("films"));
   }
+
   return films;
+};
+Storage.prototype.deleteFilmFromStorage = function (filmTitle) {
+  let films = this.getFilmsFromStorage();
+  // splice
+  films.forEach(function (film, index) {
+    if (film.title === filmTitle) {
+      films.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem("films", JSON.stringify(films));
+};
+Storage.prototype.clearAllFilmsFromStorage = function () {
+  localStorage.removeItem("films");
 };
