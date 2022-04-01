4925a20c0bf8464340dd97fd8f1fa46de1aa377b
Ezgiackgz <75390977+Ezgiackgz@users.noreply.github.com>
2022-04-01 16:44:53 +0300

    local storage

---
+++ local.js
@@ -0,0 +1,27 @@
+function Storage() {}
+Storage.prototype.addFilmToStorage = function (newFilm) {
+  // console.log(newFilm);
+
+  let films = this.getFilmsFormStorage();
+  films.push(newFilm);
+  //film objeyi diziye ataık örneğin
+  /*
+  [
+      {title:"asdsads",director:"sdfsdf",url:"sdfsdf"},
+      {title:"asdsads",director:"sdfsdf",url:"sdfsdf"}
+
+  ]
+  */
+  localStorage.setItem("films", JSON.stringify(films));
+};
+Storage.prototype.getFilmsFormStorage = function () {
+  let films;
+  if (localStorage.getItem("films") === null) {
+    //eğer films adında bir key yoksa boş bir dizi oluştur
+    films = [];
+  } else {
+    //var ise json formatında dizi haline çevirerek al
+    films = JSON.parse(localStorage.getItem("films"));
+  }
+  return films;
+};
